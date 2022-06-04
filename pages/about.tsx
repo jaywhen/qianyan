ffbefe940176ba1ff08f56d1bd55b4234fffaeff
jaywhen <jaywhen@qq.com>
2022-06-04 23:08:12 +0800

    add about

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+const About: NextPage = () => {
+  return (
+    <div className="font-serif text-xl">
+      <span className="text-3xl mr-6">qian&apos;yan</span>
+      <ruby className="text-3xl">千言<rt className="font-sans">qiān yán </rt></ruby>,
+      是一个为您生成书、影、音内容卡片的工具，目前还处于开发阶段，后续会支持更多卡片模板！
+    </div>
+  )
+}
+
+export default About;

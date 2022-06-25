0dead236f582d39dd6f6b8180c42a6f824747ae7
jaywhen <jaywhen@qq.com>
2022-06-25 17:18:34 +0800

    add splitbee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { NextPage } from 'next'
 import { ReactElement, ReactNode } from 'react'
 import CommonLayout from '@components/layouts/CommonLayout'
+import Script from 'next/script'
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -16,7 +17,13 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: ReactElement) => <CommonLayout>{page}</CommonLayout>)
   return (
     getLayout(
-      <Component {...pageProps} />
+      <>
+        <Script
+          strategy='afterInteractive'
+          src='https://cdn.splitbee.io/sb.js'
+        />
+        <Component {...pageProps} />
+      </>
     )
   )
 }
